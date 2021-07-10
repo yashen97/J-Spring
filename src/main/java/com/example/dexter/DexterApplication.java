a3d918e724741c9ecfb8519601b2811cab7d746c
yashen97 <yashenraveesha97@gmail.com>
2021-07-10 22:44:31 +0530

    first commit

---
+++ src/main/java/com/example/dexter/DexterApplication.java
@@ -0,0 +1,13 @@
+package com.example.dexter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DexterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DexterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dexter/DexterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dexter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DexterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
